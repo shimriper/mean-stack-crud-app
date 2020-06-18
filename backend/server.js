e4ebadb97cdec806bbe3dde45591e815615b7cab
shimriper <shimriper@gmail.com>
2020-06-18 18:09:23 +0300

    finelly

--- backend/server.js
+++ backend/server.js
@@ -45,10 +45,10 @@ app.use((req, res, next) => {
 });
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/", express.static(path.join(__dirname, 'public')));
 // app.use('*', express.static(path.join(__dirname, 'public/index.html')));
 
-app.get('*', (req, res) => {
+app.use('/', (req, res, next) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
